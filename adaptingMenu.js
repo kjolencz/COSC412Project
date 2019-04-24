67896d4e77279612c27a4c299c3532a90408b525
carolineeilman <43352429+carolineeilman@users.noreply.github.com>
2019-04-24 12:32:51 -0400

    Add files via upload

---
+++ adaptingMenu.js
@@ -0,0 +1,11 @@
+type="text/javascript"
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function adaptMenu() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+} 
